5f8398ae301ccc14bf404d6421f52043dfa30b48
nt54hamnghi <hamnghi250699@gmail.com>
2025-05-06 17:09:06 -0400

    refactor(app): consolidate Home component into lib.rs and remove unused pages

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,42 @@
+use components::interface::Interface;
 use leptos::prelude::*;
-use leptos_router::components::{Route, Router, Routes};
-use leptos_router::path;
 
 // Modules
 mod components;
-mod pages;
 mod shell;
 mod stores;
-
-// Top-Level pages
-use pages::home::Home;
-use pages::not_found::NotFound;
 
 /// An app router which renders the homepage and handles 404's
 #[component]
 pub fn App() -> impl IntoView {
+    view! { <Home /> }
+}
+
+/// Default Home Page
+#[component]
+fn Home() -> impl IntoView {
     view! {
-        <Router>
-            <Routes fallback=|| view! { <NotFound /> }>
-                <Route path=path!("/") view=Home />
-            </Routes>
-        </Router>
+        <ErrorBoundary fallback=|errors| {
+            view! {
+                <div class="p-4 w-screen h-screen text-base bg-surface">
+                    <h1>"Something went wrong!"</h1>
+
+                    <p class="text-fail">"Errors: "</p>
+                    // Render a list of errors as strings - good for development purposes
+                    <ul>
+                        {move || {
+                            errors
+                                .get()
+                                .into_iter()
+                                .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                                .collect_view()
+                        }}
+
+                    </ul>
+                </div>
+            }
+        }>
+            <Interface {..} id="theme-kanagawa" />
+        </ErrorBoundary>
     }
 }
